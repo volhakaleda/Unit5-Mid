46b3ced7d9a5b564269ecf97f90066e26dc81fa9
volha <Volhakaleda@ac.c4q.nyc>
2018-01-24 20:12:32 -0500

    add refresh menu

--- app/src/main/java/com/volha/myapplication/MainActivity.java
+++ app/src/main/java/com/volha/myapplication/MainActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import java.util.List;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -55,4 +58,23 @@ public class MainActivity extends AppCompatActivity {
     });
 
   }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    MenuInflater inflater = getMenuInflater();
+    inflater.inflate(R.menu.main_menu, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    switch (item.getItemId()) {
+      case R.id.refresh:
+        pullData();
+        return true;
+      default:
+        return super.onOptionsItemSelected(item);
+    }
+  }
+
 }
