ba3efae8c73d4bee776de7eac3eea36f7cbf1fd5
volha <Volhakaleda@ac.c4q.nyc>
2018-01-24 20:32:29 -0500

    details activity

--- app/src/main/java/com/volha/myapplication/DetailActivity.java
+++ app/src/main/java/com/volha/myapplication/DetailActivity.java
@@ -2,6 +2,9 @@ package com.volha.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.squareup.picasso.Picasso;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -9,6 +12,24 @@ public class DetailActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_detail);
+
+    ImageView imageView = findViewById(R.id.image);
+    TextView name = findViewById(R.id.name);
+    TextView email = findViewById(R.id.email);
+    TextView phone = findViewById(R.id.phone);
+    TextView address1 = findViewById(R.id.address_one);
+    TextView address2 = findViewById(R.id.address_two);
+    TextView dob = findViewById(R.id.dob);
+
+    Person person = getIntent().getParcelableExtra("person");
+    Picasso.with(this).load(person.getPicture().getLarge()).into(imageView);
+
+    name.setText(person.getName().getFirst() + " " + person.getName().getLast());
+    email.setText(person.getEmail());
+    phone.setText(person.getCell());
+    address1.setText(person.getLocation().getStreet());
+    address2.setText(person.getLocation().getCity() + ", " + person.getLocation().getState() + ", " + person.getLocation().getPostcode());
+    dob.setText(person.getDob());
 
 
   }
